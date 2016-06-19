2dcf54355455c30ced20dbc0dea2b3c84d1e05fb
Raphael MC COMAS <rmc-coma@e2r5p9.42.fr>
2016-06-19 04:33:20 +0200

    Wololoooooo

--- index.php
+++ index.php
@@ -1,45 +1,9 @@
 <?PHP
 session_start();
+include_once("r0f_dbselect.php");
 ?>
 <HTML>
 <HEAD>
-<script>
-<!--
-/*
-Auto Refresh Page with Time script
-By JavaScript Kit (javascriptkit.com)
-Over 200+ free scripts here!
-*/
-
-//enter refresh time in "minutes:seconds" Minutes should range from 0 to inifinity. Seconds should range from 0 to 59
-var limit="0:05"
-
-var doctitle = document.title
-var parselimit=limit.split(":")
-	parselimit=parselimit[0]*60+parselimit[1]*1
-
-function beginrefresh(){
-if (parselimit==1)
-	window.location.reload()
-	else{
-		parselimit-=1
-		curmin=Math.floor(parselimit/60)
-		cursec=parselimitvw60
-		if (curmin!=0)
-			curtime=curmin+" minutes and "+cursec+" seconds left until page refresh!"
-		else
-			curtime=cursec+" seconds left until page refresh!"
-			document.title = doctitle + ' (' + curtime +')'
-			setTimeout("beginrefresh()",1000)
-		}
-																								}
-
-if (window.addEventListener)
-	window.addEventListener("load", beginrefresh, false)
-else if (window.attachEvent)
-	window.attachEvent("load", beginrefresh)
-</script>
-
 	<meta charset="utf-8" />
 	<TITLE>Legendary Motorsports</TITLE>
 	<link rel="stylesheet" type="text/css" href="sources/index.css" />
@@ -52,13 +16,27 @@ else if (window.attachEvent)
 		<ul id="body_menu">
 			<li id="connexion">Connexion
 				<ul id="scroll_menu">
-				<form action="create.php" method="post">
-					<table id="table_scroll_menu"><tr><td>Identifiant: </td><td><input type="text" name="login" id="login"/></td></tr>
-					<tr><td>Mot de passe: </td><td><input type="password" name="passwd" id="pass"/></td>
-					<td><input id="connexion" type="submit" name="submit" value="OK" /></td></tr>
-				</table>
-				</form>
-					<li id="new_account_link"><a href="#">Cr&eacute;er un nouveau compte</a>
+<?php
+    if (isset($_SESSION) && isset($_SESSION['logged']) && $_SESSION['logged'])
+    {
+        echo "You are logged as ".$_SESSION['logged_as']."<br /><a href='r0_logout.php'>logout</a>";
+    }
+    else
+    {
+        echo "<li id='connexion'>Connexion
+                <ul id='scroll_menu'>
+                  <form action='r0_login.php' method='post'>
+                    <table>
+                      <tr><td>Identifiant: </td><td><input type='text' name='login' id='login'/></td></tr>
+                      <tr><td>Mot de passe: </td><td><input type='password' name='password' id='pass'/></td>
+                      <td><input type='submit' name='submit' value='OK' /></td></tr>
+                    </table>
+                  </form>
+               <li id='new_account_link'><a href='#'>Cr&eacute;er un nouveau compte</a>
+                 </ul>
+            </li>";
+    }
+?>
 				</ul>
 			</li>
 		</ul>
@@ -68,10 +46,12 @@ else if (window.attachEvent)
 			<DIV>
 				<form id="sort_by_type_form" action="sort_by_type.php" method="post">
 					<select id ="sort_by_type" name="sort_by_type">
-						<option selected value="all">Tous les mod&egrave;les</option>
-						<option value="4x4">4x4</option>
-						<option value="moto">Moto</option>
-						<option value="sport">Voiture de sport</option>
+						<option selected value="all">Tous les types</option>
+<?php
+	$objects = r0f_dbselect("SELECT * FROM P_TYPE ORDER BY pt_name;");
+	foreach ($objects as $type)
+		echo "<option value='".$type['pt_id']."'>".$type['pt_name']."</option>";
+?>	
 					</select>
 					<input id ="sort_by_type_submit" type="submit" value="Trier"/>
 				</form>	
@@ -79,7 +59,7 @@ else if (window.attachEvent)
 			<DIV>
 				<form id="sort_by_form" action="sort_by.php" method="post">
 					<select id ="sort_by" name="sort_by">
-						<option selected value="prix" checked>Tri par prix</option>
+						<option selected value="name" checked>Tri par prix</option>
 						<option value="new_first" checked>Tri par nouveau en premier</option>
 						<option value="old_first" checked>Tri par ancien en premier</option>
 					</select>
@@ -87,46 +67,11 @@ else if (window.attachEvent)
 				</form>	
 			</DIV>
 		</DIV>
-		<DIV class="content"><span class="price">100000000000</span></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-		<DIV class="content"></DIV>
-	
+<?php
+	$objects = r0f_dbselect("SELECT PRODUCT.*, P_BRAND.* FROM PRODUCT, P_BRAND WHERE PRODUCT.p_brand_id = P_BRAND.pb_id ORDER BY pb_name;");
+	foreach ($objects as $car)
+		echo "<DIV class='content'><img src='".$car['p_image']."'></img><span class='carname'>".$car['pb_name']."&nbsp;".$car['p_name']."</span><span class='price'>$&nbsp;".$car['p_price']."</DIV>";
+?>	
 	</DIV>
 
 </BODY>
