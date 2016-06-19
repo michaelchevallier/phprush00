172c8825b7280caa8b0012a09e37c0a53a23900d
Michael CHEVALLIER <mchevall@e2r5p10.42.fr>
2016-06-19 03:34:44 +0200

    panier done

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ Over 200+ free scripts here!
 */
 
 //enter refresh time in "minutes:seconds" Minutes should range from 0 to inifinity. Seconds should range from 0 to 59
-var limit="0:15"
+var limit="0:05"
 
 var doctitle = document.title
 var parselimit=limit.split(":")
@@ -24,7 +24,7 @@ if (parselimit==1)
 	else{
 		parselimit-=1
 		curmin=Math.floor(parselimit/60)
-		cursec=parselimit%60
+		cursec=parselimitvw60
 		if (curmin!=0)
 			curtime=curmin+" minutes and "+cursec+" seconds left until page refresh!"
 		else
@@ -42,19 +42,20 @@ else if (window.attachEvent)
 
 	<meta charset="utf-8" />
 	<TITLE>Legendary Motorsports</TITLE>
-	<link rel="stylesheet" type="text/css" href="legendary.css" />
+	<link rel="stylesheet" type="text/css" href="sources/index.css" />
 
 </HEAD>
 <BODY>
 	<DIV id="banner">
 		<img id="banner_img" SRC = "sources/images/banner-img.png" alt="Legendary Motorsport"/>
+		<ul id="panier_menu"><a href="panier.php">Panier<img id="panier_black_menu" src="sources/images/panier_black.jpg"></a></ul>
 		<ul id="body_menu">
 			<li id="connexion">Connexion
 				<ul id="scroll_menu">
 				<form action="create.php" method="post">
-					<table><tr><td>Identifiant: </td><td><input type="text" name="login" id="login"/></td></tr>
+					<table id="table_scroll_menu"><tr><td>Identifiant: </td><td><input type="text" name="login" id="login"/></td></tr>
 					<tr><td>Mot de passe: </td><td><input type="password" name="passwd" id="pass"/></td>
-					<td><input type="submit" name="submit" value="OK" /></td></tr>
+					<td><input id="connexion" type="submit" name="submit" value="OK" /></td></tr>
 				</table>
 				</form>
 					<li id="new_account_link"><a href="#">Cr&eacute;er un nouveau compte</a>
@@ -63,16 +64,57 @@ else if (window.attachEvent)
 		</ul>
 	</DIV>
 	<DIV id="core">
-		<DIV>
-			<form id="sort_by_form" action="sort_by.php" method="post">
-				<select id ="sort_by" name="sort_by">
-					<option selected value="prix" checked>Tri par prix</option>
-					<option value="new_first" checked>Tri par nouveau en premier</option>
-					<option value="old_first" checked>Tri par ancien en premier</option>
-				</select>
-				<input id ="sort_by_submit" type="submit" value="Trier"/>
-			</form>	
+		<DIV id="forms">
+			<DIV>
+				<form id="sort_by_type_form" action="sort_by_type.php" method="post">
+					<select id ="sort_by_type" name="sort_by_type">
+						<option selected value="all">Tous les mod&egrave;les</option>
+						<option value="4x4">4x4</option>
+						<option value="moto">Moto</option>
+						<option value="sport">Voiture de sport</option>
+					</select>
+					<input id ="sort_by_type_submit" type="submit" value="Trier"/>
+				</form>	
+			</DIV>
+			<DIV>
+				<form id="sort_by_form" action="sort_by.php" method="post">
+					<select id ="sort_by" name="sort_by">
+						<option selected value="prix" checked>Tri par prix</option>
+						<option value="new_first" checked>Tri par nouveau en premier</option>
+						<option value="old_first" checked>Tri par ancien en premier</option>
+					</select>
+					<input id ="sort_by_submit" type="submit" value="Trier"/>
+				</form>	
+			</DIV>
 		</DIV>
+		<DIV class="content"><span class="price">100000000000</span></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
+		<DIV class="content"></DIV>
 		<DIV class="content"></DIV>
 		<DIV class="content"></DIV>
 		<DIV class="content"></DIV>
