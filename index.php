efb94fcb8ff6e0a366929ab714fa9ee76dba71ae
Raphael MC COMAS <rmc-coma@e2r5p9.42.fr>
2016-06-19 06:24:02 +0200

    Emacs roolz

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?PHP
 session_start();
 include_once("r0f_dbselect.php");
+
+if ($_GET)
+{
+	if ($_GET['type_filter'])
+		$_SESSION['type_filter'] = $_GET['type_filter'];
+	if ($_GET['sort_by'])
+		$_SESSION['sort_by'] = $_GET['sort_by'];
+}
 ?>
 <HTML>
 <HEAD>
@@ -23,7 +31,7 @@ include_once("r0f_dbselect.php");
     }
     else
     {
-        echo "<li id='connexion'>Connexion
+        echo "<li id='connexion'>
                 <ul id='scroll_menu'>
                   <form action='r0_login.php' method='post'>
                     <table>
@@ -44,31 +52,69 @@ include_once("r0f_dbselect.php");
 	<DIV id="core">
 		<DIV id="forms">
 			<DIV>
-				<form id="sort_by_type_form" action="sort_by_type.php" method="post">
-					<select id ="sort_by_type" name="sort_by_type">
-						<option selected value="all">Tous les types</option>
+				<form id="sort_by_type_form" action="#" method="get">
+					<select id="sort_by_type" name="type_filter">
 <?php
 	$objects = r0f_dbselect("SELECT * FROM P_TYPE ORDER BY pt_name;");
+	if ($_SESSION['type_filter'] != -1)
+		echo "<option value='-1'>Tous les types</option>";
+	else
+		echo "<option selected value='-1'>Tous les types</option>";
 	foreach ($objects as $type)
-		echo "<option value='".$type['pt_id']."'>".$type['pt_name']."</option>";
+	{
+		if ($_SESSION['type_filter'] && $_SESSION['type_filter'] == $type['pt_id'])
+			echo "<option selected value='".$type['pt_id']."'>".$type['pt_name']."</option>";
+		else
+			echo "<option value='".$type['pt_id']."'>".$type['pt_name']."</option>";
+	}
 ?>	
 					</select>
-					<input id ="sort_by_type_submit" type="submit" value="Trier"/>
+					<input id="sort_by_type_submit" type="submit" value="order"/>
 				</form>	
 			</DIV>
 			<DIV>
-				<form id="sort_by_form" action="sort_by.php" method="post">
-					<select id ="sort_by" name="sort_by">
-						<option selected value="name" checked>Tri par prix</option>
-						<option value="new_first" checked>Tri par nouveau en premier</option>
-						<option value="old_first" checked>Tri par ancien en premier</option>
+				<form id="sort_by_form" action="#" method="get">
+					<select id="sort_by" name="sort_by">
+<?php
+		if ($_SESSION['sort_by'])
+		{
+			if ($_SESSION['sort_by'] == "name")
+			{
+				echo "<option selected value='name' checked>Tri par nom</option><option value='price' checked>Tri par prix croissant</option><option value='rprice' checked>Tri par prix d&eacute;croissant</option>";
+				$order = "pb_name, p_name";
+			}
+			else if ($_SESSION['sort_by'] == "price")
+			{
+				echo "<option value='name' checked>Tri par nom</option><option selected value='price' checked>Tri par prix croissant</option><option value='rprice' checked>Tri par prix d&eacute;croissant</option>";
+				$order = "p_price, p_name";
+			}
+			else if ($_SESSION['sort_by'] == "rprice")
+			{
+				echo "<option value='name' checked>Tri par nom</option><option value='price' checked>Tri par prix croissant</option><option selected value='rprice' checked>Tri par prix d&eacute;croissant</option>";
+				$order = "p_price desc";
+			}
+			else
+			{
+				echo "<option selected value='name' checked>Tri par nom</option><option value='price' checked>Tri par prix croissant</option><option value='rprice' checked>Tri par prix d&eacute;croissant</option>";
+				$order = "pb_name, p_name";
+			}
+		}
+		else
+		{
+			echo "<option selected value='name' checked>Tri par nom</option><option value='price' checked>Tri par prix croissant</option><option value='rprice' checked>Tri par prix d&eacute;croissant</option>";
+			$order = "pb_name, p_name";
+		}
+?>
 					</select>
-					<input id ="sort_by_submit" type="submit" value="Trier"/>
+					<input id="sort_by_submit" type="submit" value="Trier"/>
 				</form>	
 			</DIV>
 		</DIV>
 <?php
-	$objects = r0f_dbselect("SELECT PRODUCT.*, P_BRAND.* FROM PRODUCT, P_BRAND WHERE PRODUCT.p_brand_id = P_BRAND.pb_id ORDER BY pb_name;");
+	if ($_SESSION['type_filter'] == -1)
+		$objects = r0f_dbselect("SELECT PRODUCT.*, P_BRAND.* FROM PRODUCT, P_BRAND WHERE PRODUCT.p_brand_id = P_BRAND.pb_id ORDER BY ".$order.";");
+	else 
+		$objects = r0f_dbselect("SELECT PRODUCT.*, P_BRAND.* FROM PRODUCT, P_BRAND WHERE PRODUCT.p_brand_id = P_BRAND.pb_id AND PRODUCT.p_type_id = '".$_SESSION['type_filter']."' ORDER BY ".$order.";");
 	foreach ($objects as $car)
 		echo "<DIV class='content'><img src='".$car['p_image']."'></img><span class='carname'>".$car['pb_name']."&nbsp;".$car['p_name']."</span><span class='price'>$&nbsp;".$car['p_price']."</DIV>";
 ?>	
